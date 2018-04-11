e3daae364f4374b62ec0909c637a2c85aa2835c2
SGS Sandhu <sgs.sandhu@gmail.com>
2018-04-11 19:58:52 +0530

    Update 20180411142852

--- template/aione-topbar.php
+++ template/aione-topbar.php
@@ -3,22 +3,14 @@
 	<div class="row-wrapper">
 		<div class="ar">
 			<div class="ac l50 m50 s100 left-content">
-				<div id="" class="aione-topbar-item">
-					<?php if ( is_active_sidebar( 'aione-topbar-left' ) ) : ?>
-						<ul id="topbar-left">
-							<?php dynamic_sidebar( 'aione-topbar-left' ); ?>
-						</ul>
-					<?php endif; ?>
-				</div>
+				<?php if ( is_active_sidebar( 'aione-topbar-left' ) ) : ?>	
+					<?php dynamic_sidebar( 'aione-topbar-left' ); ?>	
+				<?php endif; ?>
 			</div>
 			<div class="ac l50 m50 s100 right-content">
-				<div id="" class="aione-topbar-item" >
-					<?php if ( is_active_sidebar( 'aione-topbar-right' ) ) : ?>
-						<ul id="topbar-right">
-							<?php dynamic_sidebar( 'aione-topbar-right' ); ?>
-						</ul>
-					<?php endif; ?>
-				</div>
+				<?php if ( is_active_sidebar( 'aione-topbar-right' ) ) : ?>
+					<?php dynamic_sidebar( 'aione-topbar-right' ); ?>	
+				<?php endif; ?>
 			</div>
 		</div>
 	</div>
