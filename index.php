da925ab8e3db226f1af6970d198c683649955931
sgssandhu <sgs.sandhu@gmail.com>
2018-05-12 20:05:46 +0530

    Update 20180512143546

--- index.php
+++ index.php
@@ -11,55 +11,71 @@
  *
  * @package Gutenbergtheme
  */
-
 get_header(); ?>
-
-	<main id="primary" class="site-main">
-	<div id="aione_post_content" class="aione-main aione-post-content">
-	<div class="wrapper">
-
-	<?php 
-	if ( have_posts() ) :
-
-		if ( is_home() && ! is_front_page() ) : ?>
-			<header>
-				<h1 class="page-title screen-reader-text aione-align-center m-0"><?php single_post_title(); ?></h1>
-			</header>
-
-		<?php
-		endif;
-		?>
-		<div class="ar">
-		<?php
-		/* Start the Loop */
-		while ( have_posts() ) : the_post();
-
-			/*
-				* Include the Post-Format-specific template for the content.
-				* If you want to override this in a child theme, then include a file
-				* called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				*/	
-			//get_template_part( 'template-parts/content', get_post_format() );
-			?>
-			<div class="ac">
-				<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+	<main id="aione_main" class="aione-main fullwidth">
+		<div class="wrapper">
+			<div class="aione-sidebar-left">
+				<div class="wrapper">
+					this is side bar left
+				</div>
 			</div>
-			<?php
+			<div class="aione-content">
+				<div class="wrapper">
+					<div class="aione-content-top">
+						<div class="wrapper">
+							
+						</div>
+					</div>
+					<div class="aione-page-content">
+						<div class="wrapper">
+							<?php 
+							if ( have_posts() ) :
+								if ( is_home() && ! is_front_page() ) : ?>
+									<header>
+										<h1 class="page-title screen-reader-text aione-align-center m-0"><?php single_post_title(); ?></h1>
+									</header>
+								<?php
+								endif;
+								?>
+								<div class="ar">
+								<?php
+									/* Start the Loop */
+									while ( have_posts() ) : the_post();
+										/*
+											* Include the Post-Format-specific template for the content.
+											* If you want to override this in a child theme, then include a file
+											* called content-___.php (where ___ is the Post Format name) and that will be used instead.
+											*/	
+										//get_template_part( 'template-parts/content', get_post_format() );
+										?>
+										<div class="ac">
+											<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+										</div>
+										<?php
 
-		endwhile;
-		?>
-		</div>
-		<?php
-		the_posts_navigation();
-
-	else :
-
-		get_template_part( 'template-parts/content', 'none' );
-
-	endif; ?>
-
-	</div><!-- .aione-post-content -->
-	</div><!-- .wrapper -->
+									endwhile;
+								?>
+								</div>
+								<?php
+								the_posts_navigation();
+							else :
+								get_template_part( 'template-parts/content', 'none' );
+							endif; ?>
+						</div>
+					</div>
+					<div class="aione-content-bottom">
+						<div class="wrapper">
+							
+						</div>
+					</div>		
+				</div>
+			</div>
+			<div class="aione-sidebar-right">
+				<div class="wrapper">
+					this is side bar right
+				</div>
+			</div>
+		</div><!-- .aione-post-content -->
 	</main><!-- #primary -->
 	
 <?php
