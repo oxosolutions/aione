9e148b4dfc22479ffe2918aefa555f357cff3131
sgssandhu <sgs.sandhu@gmail.com>
2018-05-21 15:57:09 +0530

    Update 20180521102709

---
+++ tabs/tab_custom_code.php
@@ -0,0 +1,18 @@
+<?php 
+
+$this->textarea(
+	'custom_css',
+	__( 'Custom CSS', 'Aione' ),
+	__( 'Insert custom css for the page.', 'Aione' )
+);
+
+$this->textarea(
+	'custom_js',
+	__( 'Custom JS', 'Aione' ),
+	__( 'Insert custom JS for the page.', 'Aione' )
+);
+
+
+
+
+?>
