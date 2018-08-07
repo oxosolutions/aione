d2d1e355bc63fef4580b79c3f82a8d61a20283c7
SGS Sandhu <sgs.sandhu@gmail.com>
2018-08-07 17:26:53 +0530

    Update

--- template-parts/content.php
+++ template-parts/content.php
@@ -5,7 +5,7 @@
 	<header class="entry-header">
 		<?php 
 		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">HELLO','</h1>' );
+			the_title( '<h1 class="entry-title">','</h1>' );
 		else :
 			the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
 		endif;
