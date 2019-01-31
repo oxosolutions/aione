3985617a5185fe2004fd4e196284f37d13a98c9e
SGS Sandhu <sgs.sandhu@gmail.com>
2019-01-31 14:58:04 +0530

    Update

--- template/aione-header-banner.php
+++ template/aione-header-banner.php
@@ -1,12 +1,10 @@
 <?php
 if( is_active_sidebar( 'aione-header-banner' )  ) : ?>
 	<div id="aione_header_banner" class="aione-header-banner">
-		<div class="aione-header-banner">
-			<div class="wrapper">
-				<?php dynamic_sidebar( 'aione-header-banner' ); ?>
-				<div class="aione-clear">	
-				</div><!-- .aione-clear -->
-			</div><!-- .wrapper -->
-		</div><!-- .aione-header-banner -->
-	</div>
+		<div class="wrapper">
+			<?php dynamic_sidebar( 'aione-header-banner' ); ?>
+			<div class="aione-clear">	
+			</div><!-- .aione-clear -->
+		</div><!-- .wrapper -->
+	</div><!-- .aione-header-banner -->
 <?php endif;
