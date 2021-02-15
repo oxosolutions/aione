9c525468c3a53c222411696e030459b7cb514f81
SGS Sandhu <sgs.sandhu@gmail.com>
2021-02-15 21:45:03 +0530

    Update

--- footer.php
+++ footer.php
@@ -230,6 +230,20 @@ $custom_js = get_aione_page_settings( $post->ID,'aione_per_page_setting', 'pyre_
 			color: '.esc_html($theme_options['submenu_link_hover_color']).';	
 			background-color: '.esc_html($theme_options['submenu_link_background_hover_color']).';
 		}
+
+		.primary-nav .aione-nav > ul > li ul.sub-menu > li > a {
+			color: '.esc_html($theme_options['submenu_link_color']).';	
+		}
+		.primary-nav .aione-nav > ul > li ul.sub-menu > li > a:hover,
+		.primary-nav .aione-nav > ul > li ul.sub-menu > li.current-menu-item > a,
+		.primary-nav .aione-nav > ul > li ul.sub-menu > li.current-menu-item > a:hover,
+		.primary-nav .aione-nav > ul > li ul.sub-menu > li.current-menu-parent > a,
+		.primary-nav .aione-nav > ul > li ul.sub-menu > li.current-menu-parent > a:hover,
+		.primary-nav .aione-nav > ul > li ul.sub-menu > li.current-page-ancestor > a,
+		.primary-nav .aione-nav > ul > li ul.sub-menu > li.current-page-ancestor > a:hover{
+			color: '.esc_html($theme_options['submenu_link_hover_color']).';	
+			background-color: '.esc_html($theme_options['submenu_link_background_hover_color']).';
+		}
 		'; 
 	}
 	
